ad15c72407ec8648c89d17851279c758005629f7
Raisa Primerova <riyuhide@gmail.com>
2019-12-24 18:15:47 -0700

    Updates

---
+++ precache-manifest.fa74d9566f05888adc079452be370a8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d8d30148b6b21682a86bbfe1cf4f5f",
+    "url": "/CloudWIS/index.html"
+  },
+  {
+    "revision": "bfe2cdb5e45c33dfc25e",
+    "url": "/CloudWIS/static/css/main.f314467b.chunk.css"
+  },
+  {
+    "revision": "bc63dc57ae86de0e4e6c",
+    "url": "/CloudWIS/static/js/2.45455278.chunk.js"
+  },
+  {
+    "revision": "bfe2cdb5e45c33dfc25e",
+    "url": "/CloudWIS/static/js/main.62cf164f.chunk.js"
+  },
+  {
+    "revision": "85cf1ab9d5c3dfbcf257",
+    "url": "/CloudWIS/static/js/runtime-main.0991662b.js"
+  },
+  {
+    "revision": "861450be3baee16104d7ae42ade0e796",
+    "url": "/CloudWIS/static/media/bg.861450be.svg"
+  },
+  {
+    "revision": "466ae14544b4616a3dc717b4e9dd16d3",
+    "url": "/CloudWIS/static/media/logo.466ae145.svg"
+  }
+]);
