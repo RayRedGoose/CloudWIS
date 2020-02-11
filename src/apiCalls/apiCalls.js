0daa1f9f95608ea5ca1ab4dc9af32d69b3e64a0c
Raisa Primerova <riyuhide@gmail.com>
2020-02-10 19:00:11 -0700

    Update for deployment

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -1,5 +1,5 @@
 export const getCurrentWeatherData = (city) => {
-  return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},us&APPID=b5c3a8148fd667e39bc1d42dbd91efc3`)
+  return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},us&APPID=b5c3a8148fd667e39bc1d42dbd91efc3`)
     .then(response => {
       if (!response.ok) {
         throw Error('Error fetching current weather data');
